cd2e8287fdc56f0c1d153fd73b87280b2f474e30
Alex <sasha_samara@bk.ru>
2021-08-07 11:20:31 +0400

    Add components: header, nav, profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div><img
+                src="https://selcdn.fedsp.com/corona/9/8800/2e45fd0c99935c56.jpeg"
+                alt="dog"/></div>
+            <div>ava+description</div>
+            <div>My posts
+                <div>New post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
